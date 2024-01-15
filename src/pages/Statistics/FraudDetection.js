55d0e23b199b74dfc6dcc58126c0b68f1e1ce4cd
Laxman Sharma <laxman.sharma@volgai.com>
2024-01-15 08:20:29 +0545

    created endpoint for fraud detection

--- src/pages/Statistics/FraudDetection.js
+++ src/pages/Statistics/FraudDetection.js
@@ -1,7 +1,13 @@
 /** @format */
 
+import {
+  CheckCircleOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
+import { Tag } from "antd";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import TableSkeleton from "../../components/Table Skeleton/TableSkeleton";
 
 const FraudDetection = () => {
   const dispatch = useDispatch();
@@ -11,7 +17,7 @@ const FraudDetection = () => {
 
   useEffect(() => {
     dispatch({
-      type: "",
+      type: "GET_FRAUD_DETECTION_DATA_REQUEST",
     });
   }, []);
   return (
@@ -28,6 +34,53 @@ const FraudDetection = () => {
                 identify anomalies in transaction data, specifically focusing on
                 the "amount" feature.
               </p>
+              {getFraudDetectionDataLoading ? (
+                <TableSkeleton />
+              ) : (
+                <table className="table table-hover  align-middle table-nowrap mb-0 mt-4">
+                  <thead>
+                    <tr className="table-light">
+                      <th>Transaction Id</th>
+                      <th>Transaction Amount</th>
+                      <th>Is Fraud ?</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {fraudDetectionData &&
+                    fraudDetectionData?.result?.length > 0 ? (
+                      fraudDetectionData?.result?.map((transaction, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{transaction.transaction_id}</td>
+                            <td>Rs {transaction.amount}</td>
+                            <td>
+                              {transaction.is_anomaly == 1 ? (
+                                <Tag
+                                  icon={<CheckCircleOutlined />}
+                                  color="success"
+                                >
+                                  Not Likely To Be Fraud
+                                </Tag>
+                              ) : (
+                                <Tag
+                                  icon={<ExclamationCircleOutlined />}
+                                  color="warning"
+                                >
+                                  Highly Likely To Be Fraud
+                                </Tag>
+                              )}
+                            </td>
+                          </tr>
+                        );
+                      })
+                    ) : (
+                      <tr style={{ textAlign: "center" }}>
+                        <td colSpan={3}>No Transaction Found !</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              )}
             </div>
           </div>
         </div>
